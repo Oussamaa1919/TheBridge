19bdf0dec5fc0b5074f6d7af7898388b07627590
Oussamaa1919 <medoussama.salhi@gmail.com>
2022-11-28 18:08:38 +0100

    create post new design

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -2,40 +2,50 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addPost } from '../../actions/post';
-
+import user from '../../img/user-1.png';
+import photo from '../../img/photo.png';
+import video from '../../img/video.png';
+import event from '../../img/event.png';
 const PostForm = ({ addPost }) => {
   const [text, setText] = useState('');
+  
+  
 
   return (
-    <div className='post-form'>
-      <div className='bg-primary p'>
-        <h3>Say Something...</h3>
-      </div>
-      <form
-        className='form my-1'
-        onSubmit={e => {
-          e.preventDefault();
-          addPost({ text });
-          setText('');
-        }}
-      >
-        <textarea
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='Create a post'
-          value={text}
-          onChange={e => setText(e.target.value)}
-          required
-        />
-        <input type='submit' className='btn btn-dark my-1' value='Submit' />
-      </form>
+    <div class="create-post">
+    <div class="create-post-input">
+      <img src={user}/>
+      <textarea rows="2"
+       name='text'
+       placeholder="Write a post"
+       value={text}
+       onChange={e => setText(e.target.value)}
+       required
+       >
+
+       </textarea>
     </div>
+    <div class="create-post-links">
+      <li><img src={photo}/>Photo</li>
+      <li><img src={video}/>Video</li>
+      <li><img src={event}/>Event</li>
+      <li>
+      <form 
+      onSubmit={e => {
+      e.preventDefault();
+      addPost({ text });
+      setText('');
+      }}>
+      <input type='submit' value='POST' /></form></li>
+
+    </div>
+  </div>
   );
 };
 
 PostForm.propTypes = {
-  addPost: PropTypes.func.isRequired
+  addPost: PropTypes.func.isRequired,
+ 
 };
 
 export default connect(
