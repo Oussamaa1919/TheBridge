16e423a6cfba8c5dde70d3041474bb18baa7302c
Oussamaa1919 <medoussama.salhi@gmail.com>
2023-01-29 17:54:29 +0100

    get all trainings & update training

--- routes/api/training.js
+++ routes/api/training.js
@@ -7,7 +7,18 @@ const Training = require('../../models/Training');
 const Admin = require('../../models/Admin');
 const checkObjectId = require('../../middleware/checkObjectId');
 
-
+// @route    GET api/trainings
+// @desc     Get all trainings
+// @access   Private
+router.get('/', adminauth, async (req, res) => {
+  try {
+    const trainings = await Training.find().sort({ date: -1 });
+    res.json(trainings);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // @route    POST api/trainings
 // @desc     Create a training
@@ -54,5 +65,47 @@ router.post(
     }
   }
 );
+
+// @route    Update api/trainings
+// @desc     Update a training
+// @access   Private
+
+router.put(
+  '/:id',
+  adminauth,checkObjectId('id'),
+  
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    try {
+      const training = new Training({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        location: req.body.location,
+        periode: req.body.periode,
+        coach: req.body.coach,
+        date: req.body.date,
+        admin: req.admin.id
+
+
+      });
+      Training.updateOne({_id: req.params.id}, training).then( 
+        ()=>res.status(201).json({
+          message: 'Training updated successfully !'
+        })
+      )
+      
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+
+
 
 module.exports = router;
