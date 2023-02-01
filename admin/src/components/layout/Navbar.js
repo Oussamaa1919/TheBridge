55f28a1e77aca001685e8b160531538a2fe7b585
Oussamaa1919 <medoussama.salhi@gmail.com>
2023-02-01 18:33:28 +0100

    navbar guest

--- admin/src/components/layout/Navbar.js
+++ admin/src/components/layout/Navbar.js
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react'
 import { connect } from 'react-redux';
 import searchimg from '../../images/search.png'
+import logo from '../../images/the-bridge-logo.png'
+
+
+
 const Navbar = ({ auth: { isAuthenticated} }) => {
 
   const authLinks = (
@@ -26,8 +30,34 @@ const Navbar = ({ auth: { isAuthenticated} }) => {
     </Fragment>
   );
   const  guestLinks = (    
-    <div>hiii</div> 
-);
+    <div>
+    <div className='navbar-left'>
+
+<h1 className='logo2'>
+ 
+   <img  src={logo} alt=''/>
+ 
+</h1>
+</div>
+
+   <div className='main-wavy'>
+   <div className='wavy'>  
+   <span style={{ '--i':1 }}>W</span>
+   <span style={{ '--i':2 }}>E</span>
+   <span style={{ '--i':3 }}>L</span>
+   <span style={{ '--i':4 }}>C</span>
+   <span style={{ '--i':6 }}>O</span>
+   <span style={{ '--i':7 }}>M</span>
+   <span style={{ '--i':8}}>E</span>
+   <span style={{ '--i':8}}><i class="fas fa-solid fa-hand-sparkles"></i></span>
+
+   </div>
+ </div>
+ </div>
+  
+   
+   
+ );
 
 
   return (
