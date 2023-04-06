4b29926b516ee4eef84680e528fe279b759b7fff
Oussamaa1919 <medoussama.salhi@gmail.com>
2023-04-06 02:59:28 +0200

    company routes

--- routes/api/company.js
+++ routes/api/company.js
@@ -201,4 +201,85 @@ router.post(
   }
 );
 
+router.get('/myinternships', companyauth, async (req, res) => {
+  try {
+    const internships = await Internship.find({ company: req.company.id });
+    res.json(internships);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.delete('/internship/:id', companyauth, async (req, res) => {
+  try {
+    // Get the internship to be deleted
+    const internship = await Internship.findById(req.params.id);
+
+    // Check if the internship exists
+    if (!internship) {
+      return res.status(404).json({ msg: 'Internship not found' });
+    }
+
+    // Check if the authenticated company is the owner of the internship
+    if (internship.company.toString() !== req.company.id) {
+      return res.status(401).json({ msg: 'Not authorized to delete this internship' });
+    }
+
+    // Delete the internship
+    await internship.remove();
+
+    res.json({ msg: 'Internship removed' });
+  } catch (err) {
+    console.error(err.message);
+
+    // Handle the case when the id provided is not a valid ObjectId
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Internship not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+router.get('/internship/:id/inscriptions', companyauth, async (req, res) => {
+  try {
+    const company = await Company.findById(req.company.id).select('-password');
+    const internship = await Internship.findById(req.params.id);
+
+    // Check if the internship belongs to the company
+    if (!internship || internship.company.toString() !== req.company.id) {
+      return res.status(401).json({ msg: 'Not authorized to view inscriptions' });
+    }
+
+    res.json(internship.inscriptions);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.delete('/internship/:id/inscription/:inscriptionId', companyauth, async (req, res) => {
+  try {
+    const company = await Company.findById(req.company.id).select('-password');
+    const internship = await Internship.findById(req.params.id);
+
+    // Check if the internship belongs to the company
+    if (!internship || internship.company.toString() !== req.company.id) {
+      return res.status(401).json({ msg: 'Not authorized to delete inscription' });
+    }
+
+    // Remove the inscription from the internship's inscriptions array
+    internship.inscriptions = internship.inscriptions.filter(
+      (inscription) => inscription._id.toString() !== req.params.inscriptionId
+    );
+
+    await internship.save();
+    res.json(internship.inscriptions);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
